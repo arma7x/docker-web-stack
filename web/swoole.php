a590d0857bbbf84293cc53c5e89272b72f3f9f22
Ahmad Malik <arma7x@live.com>
2023-04-29 00:37:05 +0800

    add swoole.php

---
+++ web/swoole.php
@@ -0,0 +1,13 @@
+<?php
+$http = new \Swoole\Http\Server("0.0.0.0", 5000);
+
+$http->on("start", function ($server) {
+    echo "Swoole http server is started at http://127.0.0.1:5000\n";
+});
+
+$http->on("request", function ($request, $response) {
+    $response->header("Content-Type", "text/plain");
+    $response->end("Hello World!\n");
+});
+
+$http->start();
